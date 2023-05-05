dadbdf1f30dcf14bf358cc3b43f4faf7931edeb7
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-05-04 19:24:52 -0600

    designing
    Finished designing the Team page. Updated the about page

--- mane-frame/src/pages/Team.js
+++ mane-frame/src/pages/Team.js
@@ -6,31 +6,31 @@ const Team = () => {
 			name: "Emily Zhang",
 			position: "Lead Photographer",
 			txt: "Emily is an accomplished equine photographer with over a decade of experience. She has a keen eye for detail and a natural talent for capturing the unique personalities of each horse she photographs. Emily is passionate about creating stunning images that tell a story and evoke a deep emotional connection with viewers.",
-			img: "",
+			img: "/assets/Emily Zhang.jpg",
 		},
 		{
 			name: "Jack Reynolds",
 			position: "Videographer",
 			txt: "Jack is a talented videographer who specializes in capturing horses in motion. With years of experience in the equine industry, Jack has a deep understanding of the movement and behavior of horses, which allows him to create captivating videos that showcase the grace and power of these magnificent animals.",
-			img: "",
+			img: "/assets/Jack Reynolds.jpg",
 		},
 		{
 			name: "Sarah Jones",
 			position: "Assistant Photographer",
 			txt: "Sarah is a skilled photographer who provides valuable support to the lead photographers at Mane Frame. She has a natural talent for composition and lighting and is dedicated to ensuring that each photo session runs smoothly and efficiently. Sarah is also an experienced horse handler, which allows her to work closely with the horses to get the best shots possible.",
-			img: "",
+			img: "/assets/Sarah Jones.jpg",
 		},
 		{
 			name: "John Lee",
 			position: "Marketing Manager",
 			txt: "John is responsible for promoting Mane Frame Photography and expanding our reach to new clients. With a background in marketing and a passion for horses, John is adept at creating compelling campaigns that resonate with our target audience. He is also an expert at social media marketing, which helps us to connect with horse lovers all around the world.",
-			img: "",
+			img: "/assets/John Lee.jpg",
 		},
 		{
 			name: "Rachel Kim",
 			position: "Customer Service Representative",
 			txt: "Rachel is our friendly and knowledgeable customer service representative who is dedicated to ensuring that each client has a positive experience with Mane Frame Photography. With a deep understanding of our services and a passion for horses, Rachel is always available to answer questions, address concerns, and provide guidance throughout the photography process.",
-			img: "",
+			img: "/assets/Rachel Kim.jpg",
 		},
 	];
 	return (
@@ -38,12 +38,12 @@ const Team = () => {
 			<h1>Meet the Team!</h1>
 			<div>
 				{team?.map((person) => (
-					<div>
-						<div>
+					<div key={person.name} className="teammember">
+						<div className="name">
 							<h2>{person.name}</h2>
 							<h3>{person.position}</h3>
 						</div>
-						<div>
+						<div className="info">
 							<img src={person.img} alt="" />
 							<p>{person.txt}</p>
 						</div>
