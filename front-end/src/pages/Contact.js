74487c8e5857839f8e431e8f336190cb2b5eaca4
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-07-20 18:18:17 -0600

    got the calendar implented

--- front-end/src/pages/Contact.js
+++ front-end/src/pages/Contact.js
@@ -1,87 +1,23 @@
 import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 const Contact = () => {
 	return (
 		<div>
 			<h1>Contact Us</h1>
 			<div className="flex">
-
-				
-				<div className="calender">
-					<div>
-						{/* Temp until I actually get the calender working */}
-						<table bgcolor="black" cellSpacing="21" cellPadding="21">
-							<thead>
-								<tr>
-									<th>Sun</th>
-									<th>Mon</th>
-									<th>Tue</th>
-									<th>Wed</th>
-									<th>Thu</th>
-									<th>Fri</th>
-									<th>Sat</th>
-								</tr>
-							</thead>
-
-							<tbody>
-								<tr>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td>1</td>
-									<td>2</td>
-								</tr>
-								<tr></tr>
-								<tr>
-									<td>3</td>
-									<td>4</td>
-									<td>5</td>
-									<td>6</td>
-									<td>7</td>
-									<td>8</td>
-									<td>9</td>
-								</tr>
-								<tr>
-									<td>10</td>
-									<td>11</td>
-									<td>12</td>
-									<td>13</td>
-									<td>14</td>
-									<td>15</td>
-									<td>16</td>
-								</tr>
-								<tr>
-									<td>17</td>
-									<td>18</td>
-									<td>19</td>
-									<td>20</td>
-									<td>21</td>
-									<td>22</td>
-									<td>23</td>
-								</tr>
-								<tr>
-									<td>24</td>
-									<td>25</td>
-									<td>26</td>
-									<td>27</td>
-									<td>28</td>
-									<td>29</td>
-									<td>30</td>
-								</tr>
-								<tr>
-									<td>31</td>
-									<td>1</td>
-									<td>2</td>
-									<td>3</td>
-									<td>4</td>
-									<td>5</td>
-									<td>6</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
+				<div className="calenderDiv">
+					<FullCalendar className="calender"
+						plugins={[dayGridPlugin]}
+						initialView="dayGridMonth"
+						weekends={true}
+						events={[
+							{ title: "event 1", date: "2023-07-04" },
+							{ title: "event 2", date: "2023-07-07" },
+						]
+					}
+					/>
 				</div>
 
 				<div className="form">
@@ -127,8 +63,11 @@ const Contact = () => {
 						</h4>
 					</div>
 				</div>
-				</div>
 			</div>
+			<br />
+			<br />
+			<br />
+		</div>
 	);
 };
 
