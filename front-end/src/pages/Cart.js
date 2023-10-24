9b33ea6c066afe4b0dd1901d85bf9fc9d332cbe2
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-10-24 13:03:51 -0600

    updated cart

--- front-end/src/pages/Cart.js
+++ front-end/src/pages/Cart.js
@@ -10,6 +10,7 @@ const Cart = () => {
 	const [user, setUser] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const [msg, setMsg] = useState(null);
+	const [errorMsg, setErrorMsg] = useState(null);
 	let email = null;
 
 	useEffect(() => {
@@ -32,7 +33,6 @@ const Cart = () => {
 			.then((data) => data.json())
 			.then((data) => {
 				setCart(data[0]);
-				
 			})
 			.catch((err) => console.log(err));
 	}
@@ -57,15 +57,24 @@ const Cart = () => {
 				const productId = product.ProductID;
 				ids.push(productId);
 			}
-
-			let getUrl = `https://mane-frame-backend.onrender.com/findProduct/${ids}`;
+			if (ids.length > 0) {
+				let getUrl = `https://mane-frame-backend.onrender.com/findProduct/${ids}`;
 			fetch(getUrl)
 				.then((data) => data.json())
 				.then((data) => {
 					setProducts(data);
-					setLoading(false)
+					setLoading(false);
 				})
-				.catch((err) => console.log(err));
+				.catch((err) => {
+					setErrorMsg(
+						"An error has occurred! Please come back later or contact us about the problem!"
+					);
+					console.log(err)
+					setLoading(false);
+				});
+			}else{
+				setProducts(null)
+			}
 		}
 	}
 
@@ -144,74 +153,80 @@ const Cart = () => {
 						</div>
 					) : (
 						<>
-							{cart && products ? (
-								<>
-									<br />
-									<main>
-										<div className="row g-5 ">
-											<div>
-												<h1 className="mb-3 cartPg">Your cart</h1>
-												{msg ? <h2>{msg}</h2> : <></>}
-												<ul className="list-group mb-3 cart">
-													{cart ? (
-														products?.map((product) => (
-															<li
-																key={product.Name}
-																className="list-group-item d-flex justify-content-between lh-sm"
-															>
-																<div>
-																	<h4 className="my-0">{product.Name}</h4>
-																	<small>{product.BriefDescription}</small>
-																	<br />
-																	{cart.Products.ProductID}
-																	{/* add something to decease the qty */}
-																	<small>
-																		Qty: {getProductQty(product._id)}
-																	</small>
-																</div>
-
-																<span className="prices">
-																	$
-																	{prices(
-																		getProductQty(product._id),
-																		product.Price
-																	)}
-																</span>
-																{/* delete item no matter the qty*/}
-																<svg
-																	onClick={() =>
-																		deleteItem(cart.UserID, product._id)
-																	}
-																	xmlns="http://www.w3.org/2000/svg"
-																	width={"35"}
-																	fill="#C41010"
-																	className="bi bi-cart-x-fill"
-																	viewBox="0 0 16 16"
-																>
-																	<path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7.354 5.646 8.5 6.793l1.146-1.147a.5.5 0 0 1 .708.708L9.207 7.5l1.147 1.146a.5.5 0 0 1-.708.708L8.5 8.207 7.354 9.354a.5.5 0 1 1-.708-.708L7.793 7.5 6.646 6.354a.5.5 0 1 1 .708-.708z" />
-																</svg>
-															</li>
-														))
-													) : (
-														<></>
-													)}
-													<li className="list-group-item d-flex justify-content-between">
-														<h4>Total (USD)</h4>
-														<strong className="prices">${total()}</strong>
-													</li>
-												</ul>
-												<div className="checkoutBtn">
-													<Link to={"/checkout"} className="btn">
-														<button>Proceed to Checkout</button>
-													</Link>
-												</div>
-											</div>
-										</div>
-									</main>
-								</>
+							{errorMsg ? (
+								<div className="error">{errorMsg}</div>
 							) : (
 								<>
-									<h1>Your Cart is empty!</h1>
+									{cart && products ? (
+										<>
+											<br />
+											<main>
+												<div className="row g-5 ">
+													<div>
+														<h1 className="mb-3 cartPg">Your cart</h1>
+														{msg ? <h2>{msg}</h2> : <></>}
+														<ul className="list-group mb-3 cart">
+															{cart ? (
+																products?.map((product) => (
+																	<li
+																		key={product.Name}
+																		className="list-group-item d-flex justify-content-between lh-sm"
+																	>
+																		<div>
+																			<h4 className="my-0">{product.Name}</h4>
+																			<small>{product.BriefDescription}</small>
+																			<br />
+																			{cart.Products.ProductID}
+																			{/* add something to decease the qty */}
+																			<small>
+																				Qty: {getProductQty(product._id)}
+																			</small>
+																		</div>
+
+																		<span className="prices">
+																			$
+																			{prices(
+																				getProductQty(product._id),
+																				product.Price
+																			)}
+																		</span>
+																		{/* delete item no matter the qty*/}
+																		<svg
+																			onClick={() =>
+																				deleteItem(cart.UserID, product._id)
+																			}
+																			xmlns="http://www.w3.org/2000/svg"
+																			width={"35"}
+																			fill="#C41010"
+																			className="bi bi-cart-x-fill"
+																			viewBox="0 0 16 16"
+																		>
+																			<path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7.354 5.646 8.5 6.793l1.146-1.147a.5.5 0 0 1 .708.708L9.207 7.5l1.147 1.146a.5.5 0 0 1-.708.708L8.5 8.207 7.354 9.354a.5.5 0 1 1-.708-.708L7.793 7.5 6.646 6.354a.5.5 0 1 1 .708-.708z" />
+																		</svg>
+																	</li>
+																))
+															) : (
+																<></>
+															)}
+															<li className="list-group-item d-flex justify-content-between">
+																<h4>Total (USD)</h4>
+																<strong className="prices">${total()}</strong>
+															</li>
+														</ul>
+														<div className="checkoutBtn">
+															<Link to={"/checkout"} className="btn">
+																<button>Proceed to Checkout</button>
+															</Link>
+														</div>
+													</div>
+												</div>
+											</main>
+										</>
+									) : (
+										<>
+											<h1>Your Cart is empty!</h1>
+										</>
+									)}
 								</>
 							)}
 						</>
