c9ad44a024ea60c925f9071b651161389b4bbc1b
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-03-28 11:49:51 -0600

    get the static pictures shown

--- front-end/src/pages/Client/ClientPics.js
+++ front-end/src/pages/Client/ClientPics.js
@@ -22,23 +22,48 @@ const ClientPics = () => {
 	}, []);
 
 	const [user, setUser] = useState(null);
-	const [pics, setPics] = useState(null);
+	// const [pics, setPics] = useState(null);
+	const pics = [
+		{
+			name: "pic1",
+			url: "/assets/temp/pic1.jpg",
+		},
+		{
+			name: "pic2",
+			url: "/assets/temp/pic2.jpg",
+		},
+		{
+			name: "pic3",
+			url: "/assets/temp/pic3.jpg",
+		},
+		{
+			name: "pic4",
+			url: "/assets/temp/pic4.jpg",
+		},
+		{
+			name: "pic5",
+			url: "/assets/temp/pic5.jpg",
+		},
+		{
+			name: "pic6",
+			url: "/assets/temp/pic6.jpg",
+		},
+	];
+	// function loadAPI() {
+	// 	let getUrl = `https://mane-frame-backend.onrender.com/findUserEmail/${email}`;
+	// 	fetch(getUrl)
+	// 		.then((data) => data.json())
+	// 		.then((data) => {
+	// 			setUser(data.User);
+	// 			setPics(data.User.Images);
+	// 			setLoading(false);
+	// 		})
+	// 		.catch((err) => console.log(err));
+	// }
 
-	function loadAPI() {
-		let getUrl = `https://mane-frame-backend.onrender.com/findUserEmail/${email}`;
-		fetch(getUrl)
-			.then((data) => data.json())
-			.then((data) => {
-				setUser(data.User);
-				setPics(data.User.Images);
-				setLoading(false);
-			})
-			.catch((err) => console.log(err));
-	}
-
-	useEffect(() => {
-		loadAPI();
-	}, [email]);
+	// useEffect(() => {
+	// 	loadAPI();
+	// }, [email]);
 
 	function downloadImage(url, name) {
 		saveAs(url, name);
@@ -81,7 +106,22 @@ const ClientPics = () => {
 				// 		<br />
 				// 	</>
 				// )
-				<h2 className="error">This page currently isn't working. Please give the team some time to fix it!</h2>
+				// <h2 className="error">This page currently isn't working. Please give the team some time to fix it!</h2>
+				<>
+					{pics && pics.length > 0 ? (
+						<h1>Your Pictures</h1>
+					) : (
+						<h1>No pictures to show right now</h1>
+					)}
+					<div className="pics">
+						{pics?.map((pic) => (
+							<a onClick={() => downloadImage(pic.url, pic.name)}>
+								<img src={pic.url} />
+							</a>
+						))}
+					</div>
+					<br />
+				</>
 			) : (
 				<>
 					<h1>You don't have permission to view this page</h1>
